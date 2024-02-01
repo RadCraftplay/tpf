b0ff3d7f10d9fe156c481ed11e969cfc093a264f
RadCraftplay <radcraftplay2@gmail.com>
2024-02-01 15:26:03 +0100

    Added page for testing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {TasksDashboardComponent} from "./tasks-dashboard/tasks-dashboard.compone
 import {TaskDetailsComponent} from "./task-details/task-details.component";
 import {TaskCreatorComponent} from "./task-creator/task-creator.component";
 import { HomeComponent } from './home/home.component';
+import { ComponentTestsComponent } from './component-tests/component-tests.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'dashboard', component: TasksDashboardComponent },
   { path: 'detail/:id', component: TaskDetailsComponent },
   { path: 'create', component: TaskCreatorComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'test', component: ComponentTestsComponent },
 ];
 
 @NgModule({
---
+++ src/app/component-tests/component-tests.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { ListSelectorComponent } from '../list-selector/list-selector.component';
+
+@Component({
+  selector: 'app-component-tests',
+  standalone: true,
+  imports: [],
+  templateUrl: './component-tests.component.html',
+  styleUrl: './component-tests.component.css'
+})
+export class ComponentTestsComponent {
+  
+}
