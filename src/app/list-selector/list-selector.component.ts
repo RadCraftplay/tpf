d35de5092986deda2bf56491018297f35003bd69
RadCraftplay <radcraftplay2@gmail.com>
2024-02-02 15:55:21 +0100

    [WIP] Added list selector component

---
+++ src/app/list-selector/list-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-selector.component.html',
+  styleUrl: './list-selector.component.css'
+})
+export class ListSelectorComponent {
+
+}
