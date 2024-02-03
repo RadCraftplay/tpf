6ccbe17604312df22fe6f784ad2c20f95a17a6e6
Adrian Ciesielczyk <adrianciesielczyk2000@gmail.com>
2024-02-03 19:01:07 +0100

    Task details view added

--- src/app/task-details/task-details.component.ts
+++ src/app/task-details/task-details.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../services/tasks-service/task';
+import { ActivatedRoute } from "@angular/router";
+import { TasksService } from "../services/tasks-service/tasks-service";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.css'
 })
-export class TaskDetailsComponent {
+export class TaskDetailsComponent implements OnInit {
+  task: Task | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private tasksService: TasksService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getTask();
+  }
+
+  getTask(): void {
+    const id : string | null = this.route.snapshot.paramMap.get('id')
+    if (id !== null) {
+      this.tasksService.getTaskById(id)
+        .subscribe(task => {
+          task.id = id
+          this.task = task
+        });
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.task) {
+      this.tasksService.updateTask(this.task)
+        .then(task => {
+          this.goBack();
+        });
+    }
+  }
 }
